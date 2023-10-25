03eb8321cb09c4d18de42e7784b9fa7d616fc96e
Vanzades <147304427+Vanzades@users.noreply.github.com>
2023-10-25 16:17:48 +0700

    Create Tugas_variabel.py

---
+++ Tugas_variabel.py
@@ -0,0 +1,11 @@
+#tugas1pertemuan5
+
+motor= ["beat karbu", "matic", "140cc", "hitam", "roda dua"]
+print(motor)
+
+motor.append("15.000.000")
+motor.append("sepada motor")
+print(motor)
+
+motor.insert(2, "Honda")
+print(motor)
